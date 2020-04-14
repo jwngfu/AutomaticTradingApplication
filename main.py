28fe6636ca489d07941cd13369f927a65a7a863c
Jwngfu Brahma <open.jwngfu@gmail.com>
2020-04-14 23:15:16 +0530

    Multithreading Added
    Now multiple orders can be monitored simultaneously.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+#Suppoting programs
 import readfile
 import timefunction as tm
 import order
 
-order_time = []
+#Library
+from threading import Thread
 
-f = readfile.read(input('Enter the path: '))
+#Variables
+order_time = []
+threads = []
+
+#Attempt to read the input file
+try:
+    f = readfile.read(input('Enter the path: '))
+    number_of_rows = len(list(f))
+except:
+    print("Sorry, the input file couldn't be opened.")
+    exit(0)
 
 #Place Order
 def placeOrder():
     for cell in f:
         #Place order
-        order.place(cell[5],cell[7], cell[4], 123, 1234) #testing only
+        #Cell number starts from zero
+        order.place(cell[5],cell[7], cell[4], 123, 1234)
         #Records the order time in timestamp format
         order_time.append(tm.now())
     print("\n")
@@ -18,7 +31,7 @@ def placeOrder():
 
 #monitor order
 def monitor(i):
-    pass
+    print("Monitoring order number", i+1) #testing
 
 #confirm order
 def confirmOrder(i):
@@ -30,8 +43,22 @@ def confirmOrder(i):
         monitor(i)
 
 def main():
+    #Print the number of orders to work with
+    print("Number of orders in the file : ", number_of_rows)
+
+    #Place the Order
     placeOrder()
-    confirmOrder(1) #testing only
+    
+    #Attemp the order
+    for row in range(number_of_rows):
+        #Multithreading to monitor multiple orders simultaneously
+        process = Thread(target=confirmOrder, args=(row,))
+        process.start()
+        threads.append(process)
+    
+    #joining the threads before exiting main()
+    for process in threads:
+        process.join()
 
 if __name__ == "__main__":
     main()
